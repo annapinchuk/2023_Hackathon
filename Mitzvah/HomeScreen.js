e9df44ada1555651a8ed22efbfbc2202ce94e24e
mosh <moshebuyum11@gmail.com>
2023-05-15 13:15:58 +0300

    ScrollView and items

--- Mitzvah/HomeScreen.js
+++ Mitzvah/HomeScreen.js
@@ -2,40 +2,16 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
+import Item from "./Item.js";
 const HomeScreen = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 1</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 2</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 3</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 4</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 5</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 6</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 7</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 8</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 9</Text>
-      </View>
-      <View style={styles.item}>
-        <Text style={styles.text}>Item 10</Text>
-      </View>
-    </ScrollView>
+  <Item title="Item 1" description="Description for Item 1" />
+  <Item title="Item 2" description="Description for Item 2" />
+  <Item title="Item 3" description="Description for Item 3" />
+  
+</ScrollView>
+
   );
 };
 
